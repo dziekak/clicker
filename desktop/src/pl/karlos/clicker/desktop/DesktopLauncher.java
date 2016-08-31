e4ee98cdb7a235d4190c4de3cee032f5a7e9f2dc
QQ\Karol <dziekak@gmail.com>
2016-08-31 18:22:06 +0200

    init

---
+++ desktop/src/pl/karlos/clicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.karlos.clicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.karlos.clicker.Clicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Clicker(), config);
+	}
+}
